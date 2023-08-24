fbf38ae35af8720e3ae37f985c6768bfc1114371
Alimkbn1 <mkbn.consultancy@gmail.com>
2023-08-24 12:28:12 -0700

    C - printf

--- _printf.c
+++ _printf.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void flush_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * my_printf - Custom printf function
+ * @format: Format string
+ * Return: Number of printed characters
  */
-int _printf(const char *format, ...)
+int my_printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
@@ -25,19 +25,18 @@ int _printf(const char *format, ...)
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+				flush_buffer(buffer, &buff_ind);
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
+			flush_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = process_format(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -45,7 +44,7 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	flush_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
@@ -53,11 +52,11 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * flush_buffer - Print the contents of the buffer and reset index
+ * @buffer: Char array
+ * @buff_ind: Buffer index
  */
-void print_buffer(char buffer[], int *buff_ind)
+void flush_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
